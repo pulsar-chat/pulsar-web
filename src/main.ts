d5a60e4f68eabbde01e98c98ad8fc0ddbf02b994
MalyshevMS <matvey.malyshev29@gmail.com>
2026-01-18 22:34:58 +0300

    сделал минимальный базовый клиент

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
+import PulsarWebSocket from "./websocket/pulsar";
+
 console.log("Hello, World!");
+
+const pwsURL = 'ws://212.113.98.14:8080';
+const pws = new PulsarWebSocket(pwsURL);
+
+pws.connect();
+
+pws.onOpen = () => {
+    pws.send("Hello, World!");
+}
---
+++ src/websocket/pulsar.ts
@@ -0,0 +1,67 @@
+class PulsarWebSocket {
+    private url: string;
+    private socket: WebSocket | null = null;
+    private reconnectInterval: number = 2000;
+    private shouldReconnect: boolean = true;
+
+    public onOpen: (() => void) | null = null;
+    public onClose: ((ev: CloseEvent) => void) | null = null;
+    public onError: ((ev: Event) => void) | null = null;
+    public onMessage: ((raw: string) => void) | null = null;
+
+    constructor(url: string) {
+        this.url = url;
+    }
+
+    connect() {
+        if (
+            this.socket && (
+                this.socket.readyState === WebSocket.OPEN || 
+                this.socket.readyState === WebSocket.CONNECTING
+            )
+        ) return;
+
+        this.shouldReconnect = true;
+        this.socket = new WebSocket(this.url);
+        this.socket.binaryType = "arraybuffer";
+
+        this.socket.addEventListener("open", () => {
+            this.onOpen && this.onOpen();
+        }); // обработка открытия сокета
+
+        this.socket.addEventListener("close", (ev) => {
+            this.onClose && this.onClose(ev);
+            this.socket = null;
+            if (this.shouldReconnect) setTimeout(() => this.connect(), this.reconnectInterval);
+        }); // обработка закрытия сокета и реконнект
+
+        this.socket.addEventListener("error", (ev) => {
+            this.onError && this.onError(ev);
+        }); // обработка ошибок?
+
+        this.socket.addEventListener("message", (ev) => {
+            const bytes = new Uint8Array(ev.data);
+            const raw = new TextDecoder().decode(bytes);
+
+            this.onMessage && this.onMessage(raw);
+        }); // обработка сообщений
+    }
+
+    disconnect() {
+        this.shouldReconnect = false;
+        if (this.socket) {
+            this.socket.close(0, "disconnect (PulsarWebSocket::disconnect)");
+            this.socket = null;
+        }
+    }
+
+    send(raw: string) {
+        if (!this.socket || this.socket.readyState !== WebSocket.OPEN) {
+            throw new Error("PulsarWebSocket is not open!");
+        }
+
+        this.socket.send(raw);
+    }
+}
+
+export default PulsarWebSocket;
