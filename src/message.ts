daaae093e337f95fac5864417541bbb932b980e6
rewnant <ss.ost@mail.ru>
2026-01-29 21:34:10 +0300

    полностью сделан класс message + defines

--- src/message.ts
+++ src/message.ts
@@ -3,12 +3,11 @@
 function stringify(arg: unknown): string {
     return String(arg);
 }
-// в тс ес что стро
+
 function removeSpaces(s: string): string {
-    return s.replace(/\s+/g, "");//\s это юлбой пробельный символ типо таб или ентер а g это мы
-    //  тип по всей строке чекаем(global) а не ток да первого совпадения \s
+    return s.replace(/\s+/g, "");
 }
-function alignRight(size: number,arg: unknown,fill: string = ' '): string {
+function alignRight(size: number, arg: unknown, fill: string = ' '): string {
     const str = stringify(arg);
     const sSize = str.length;
     const delta = size - sSize;
@@ -27,26 +26,12 @@ class Message {
     private sender: string;
     private timestamp: Date;
 
-    constructor(){// если ничгео не подаем в конструктор(поумолчаниб)
-        this.id = 0;
-        this.content = "";
-        this.receiver = "";
-        this.sender = "";
-        this.timestamp = new Date(); //в школе рассксжу
-    };
     constructor(id: number, content: string, receiver: string, sender: string, timestamp: Date) {
         this.id = id;
         this.content = content;
         this.receiver = receiver;
         this.sender = sender;
         this.timestamp = timestamp;
-    }
-    constructor(id: number, content: string, receiver: string, sender: string){
-         this.id = id;
-        this.content = content;
-        this.receiver = receiver;
-        this.sender = sender;
-        this.timestamp = new Date();//тк дату не подаем ставим текущую
     }
     getId(): number {
         return this.id;
@@ -67,33 +52,30 @@ class Message {
     getContent(): string {
         return this.content;
     }
-    // std::string to_payload() const {
-    //     std::ostringstream oss;
-
-    //     oss << align_right(PULSAR_ID_SIZE, id, '0');
-    //     oss << align_right(PULSAR_TIME_SIZE, time.toTime(), '0');
-    //     oss << align_right(PULSAR_SRC_SIZE, src);
-    //     oss << align_right(PULSAR_DST_SIZE, dst);
-    //     oss << msg;
-
-    //     return oss.str();
-    // }я н е понял что  это делает(типо в конечную форму предачина серв???)
 
 
+    toPayload(): string {
+        let payload = "";
+        payload += alignRight(PULSAR_ID_SIZE, this.id, "0");
+        payload += alignRight(PULSAR_TIME_SIZE, this.timestamp, "0");
+        payload += alignRight(PULSAR_SRC_SIZE, this.sender);
+        payload += alignRight(PULSAR_DST_SIZE, this.receiver);
+        payload += this.content;
+        return payload;
+    }
 
+    static toPayload(message: Message): string {
+        return message.toPayload();
+    }
 
-    // toPayload(): string {
-    //     let payload = "";
-    //     payload += alignRight(PULSAR_ID_SIZE, this.id, "0");
-    //     payload += alignRight(PULSAR_TIME_SIZE, this.time.toTime(), "0");
-    //     payload += alignRight(PULSAR_SRC_SIZE, this.src);
-    //     payload += alignRight(PULSAR_DST_SIZE, this.dst);
-    //     payload += this.msg;
-    //     return payload;
-    // }
-
-    // static toPayload(message: Message): string {
-    //     return message.toPayload();
-    // }
-
+    fromPayload(payload: string): Message {
+        const idStr = removeSpaces(payload.slice(0, PULSAR_ID_SIZE));
+        const timeStr = removeSpaces(payload.slice(PULSAR_ID_SIZE, PULSAR_ID_SIZE + PULSAR_TIME_SIZE));
+        const senderStr = removeSpaces(payload.slice(PULSAR_ID_SIZE + PULSAR_TIME_SIZE, PULSAR_ID_SIZE + PULSAR_TIME_SIZE + PULSAR_SRC_SIZE));
+        const receiverStr = removeSpaces(payload.slice(PULSAR_ID_SIZE + PULSAR_TIME_SIZE + PULSAR_SRC_SIZE, PULSAR_ID_SIZE + PULSAR_TIME_SIZE + PULSAR_SRC_SIZE + PULSAR_DST_SIZE));
+        const contentStr = payload.slice(PULSAR_ID_SIZE + PULSAR_TIME_SIZE + PULSAR_SRC_SIZE + PULSAR_DST_SIZE);
+        const id = parseInt(idStr, 10);
+        const timestamp = new Date(parseInt(timeStr, 10));
+        return new Message(id, contentStr, receiverStr, senderStr, timestamp);
+    }
 }
